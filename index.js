58dab781dbc96babeff2846c5e711980f8286a67
root <root@Kasey-TCHKAO1Q.localdomain>
2022-10-10 22:57:02 -0400

    commit tree 131e5cb74ec56059f102c6292d571f58dcc0df71

---
+++ index.js
@@ -0,0 +1,20 @@
+//Basic Math
+let number= 17
+function add(x,y){
+    return (x+y)}
+function subtract(x,y){
+    return (x-y)}
+function multiply(x,y){
+    return (x*y)}
+function divide(x,y){
+    return(x/y)}
+function increment(number){
+    if (number+"++") ("++"+number)
+    return(number+1)}
+function decrement(number){
+    if (number+"--") ("--"+number)
+    return(number-1)}
+function makeInt(x){
+    return parseInt(x,10)}
+function preserveDecimal(x){
+    return parseFloat(x,10)}
